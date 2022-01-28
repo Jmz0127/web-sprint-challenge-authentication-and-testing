c62a119a67c5a91e6100a4c281d61b8b792f3df7
Jon_Em <45055471+Jmz0127@users.noreply.github.com>
2022-01-28 11:16:23 -0500

    fixed sanity check test, added dependencies

--- api/server.test.js
+++ api/server.test.js
@@ -1,4 +1,21 @@
 // Write your tests here
+const request = require('supertest')
+const server = require('./server')
+const db = require('../data/dbConfig')
+
+
+
+
+
 test('sanity', () => {
-  expect(true).toBe(false)
+  expect(true).not.toBe(false)
+})
+
+beforeAll(async () => {
+  await db.migrate.rollback()
+  await db.migrate.latest()
+})
+
+afterAll(async () => {
+  await db.destroy()
 })
