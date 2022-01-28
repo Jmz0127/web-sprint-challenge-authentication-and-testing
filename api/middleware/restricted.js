a5a0c28e3f8f7af2315388abd085b6187feb55d5
Jon_Em <45055471+Jmz0127@users.noreply.github.com>
2022-01-28 13:52:12 -0500

    populated restricted to validate token

--- api/middleware/restricted.js
+++ api/middleware/restricted.js
@@ -1,3 +1,7 @@
+const jwt = require('jsonwebtoken')
+const { JWT_SECRET } = require('../secrets/index')
+
+
 module.exports = (req, res, next) => {
   next();
   /*
@@ -5,10 +9,25 @@ module.exports = (req, res, next) => {
 
     1- On valid token in the Authorization header, call next.
 
+
     2- On missing token in the Authorization header,
       the response body should include a string exactly as follows: "token required".
 
     3- On invalid or expired token in the Authorization header,
       the response body should include a string exactly as follows: "token invalid".
   */
+
+      
+        const token = req.headers.authorization //On valid token in the Authorization header, call next.
+        if(!token) {
+          return next(res.json({message:"token required",status:401,})) //2
+        }
+        jwt.verify(token, JWT_SECRET, (err, decodedToken) => {
+          if(err) {
+            return next(res.status(401).json({message:"token invalid"})) //3
+          }
+          req.decodedToken = decodedToken
+          next()
+        })
+
 };
